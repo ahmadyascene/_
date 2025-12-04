6ca434ffa62367899ddbf1631c7aae0865f2765b
Ahmad Yascene <ahmadyascene@gmail.com>
2025-12-05 01:00:28 +0200

    Refactor function F into function c and add async handling

--- w.js
+++ w.js
@@ -4,7 +4,7 @@ onmessage=({data:[S,C,n,dl]})=>{
       for (const {P} of r.slice(1)) for (const [d] of P)
         if (!D.includes(d)) D.push(d);
       return D.length;},
-    F=(i,l)=>{
+    c=(S,i=0,l=[])=>{
       if (i+1==S.length) L:for (const s of S[i]) {const r=[0], P=[];
         l:for (const cs of l.concat(s)) {
           for (const p of cs.P) if (P.includes(p))
@@ -16,5 +16,12 @@ onmessage=({data:[S,C,n,dl]})=>{
             L.length=0; L.push(r);}}
         else {if (cd(r)==cd(L[0])) L.push(r);
           else if (cd(r)<(cd(L[0])||7)) {L.length=0; L.push(r);}}}
-      else for (const s of S[i]) F(i+1,l.concat(s));};
-  F(0,[]); postMessage(L);}
+      else for (const s of S[i]) c(S,i+1,l.concat(s));};
+  let N=1, I=0, bc=0;
+  async function b(S,i=0) {
+    if (i==I) for (const s of S[i]) {postMessage(bc/N*100+'%');
+      if (bc) await new Promise(t=>setTimeout(t)); c(S.toSpliced(i,1,[s])); bc++;}
+    else for (const s of S[i]) await b(S.toSpliced(i,1,[s]),i+1);};
+  for (let i=S.length; i;) {N*=S[--i].length; if (N>1e5&&!I) {I=i; N=S[i].length;}}
+  if (!I) N=S[0].length;
+  b(S).then(()=>postMessage(L));}
